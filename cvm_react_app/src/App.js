4342171dfc06d142d3057e9ee1bd2477ef4b3b93
Piotr Sendor <petersendor@gmail.com>
2020-12-27 02:01:40 +0000

    material UI start

--- cvm_react_app/src/App.js
+++ cvm_react_app/src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
+
+import { Button } from '@material-ui/core';
 
 function App() {
-  return (
-    <div c>
-      Hello World!
-    </div>
-  );
+  return <Button color="primary">Hello World</Button>;
 }
 
 export default App;
